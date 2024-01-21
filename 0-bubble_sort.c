023bb36f8e474dd531d2be58ba274a002cee1ead
creeds-knight <apedoarthur21@gmail.com>
2024-01-21 19:37:23 +0300

    0-bubble corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,5 @@
 #include "sort.h"
 /**
- * swap - A function to swap the numbers at different indexes
- * @pos_1 - A number at the  first position
- * @pos_2 - A number at the last position
- * Return - None
- */
-void swap(int *pos_1, int *pos_2)
-{
-	int tmp = *pos_1;
-	*pos_1 = *pos_2;
-	*pos_2 = tmp;
-}
-
-/**
  * bubble_sort - A func to sort integers in ascending order using bubble sort
  * @array: Input of an array of integers
  * @size: Input size of the array
