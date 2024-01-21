7cdc5dc1bac013f395e8cf077b6dc898940e13c4
creeds-knight <apedoarthur21@gmail.com>
2024-01-21 19:44:15 +0300

    2-selection corrections

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,18 @@
 #include "sort.h"
 
 /**
+ * swap - A function to swap the numbers at different indexes
+ * @pos_1: A number at the  first position
+ * @pos_2:  A number at the last position
+ * Return - None
+ */
+void swap(int *pos_1, int *pos_2)
+{
+	int tmp = *pos_1;
+	*pos_1 = *pos_2;
+	*pos_2 = tmp;
+}
+/**
  * selection_sort - A function that uses selection of min numbers for sorting
  * @array: Input array to be sorted
  * @size: Size of the array to be sorted
